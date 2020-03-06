f6a2f6d9dd6e4226d19c6ee2f954d77fe2944480
Ovi Savescu <ovi@ovswebsites.com>
2020-03-06 16:47:35 +0200

    button url fixed

--- src/components/hp-sections/lunch.js
+++ src/components/hp-sections/lunch.js
@@ -46,7 +46,7 @@ const Sidebar = () => {
       <Styled.h1 as='h2'>Lunch Provided</Styled.h1>
       <p>To make your life a little bit easier, a nutritious and delicious lunch is provided daily. </p>
       <p>A vegetarian option is always available. Little ones get a morning snack and everyone enjoys an afternoon snack of ice cream or ice pops.</p>
-      <Link to='/' sx={{variant: 'buttons.3D', display: 'inline-block', my: 3, mr: 3, position: 'relative', pl: '3.5rem'}}>
+      <Link to='/lunch/' sx={{variant: 'buttons.3D', display: 'inline-block', my: 3, mr: 3, position: 'relative', pl: '3.5rem'}}>
         <FaHamburger sx={{position: 'absolute', top: '0.7rem', left: '1rem', fontSize: 4}} /> Sample Lunch Menu
       </Link>
       <Link to='/request-info/' sx={{variant: 'buttons.3DAccent', display: 'inline-block'}}><FaPaperPlane /> Request Info</Link>
