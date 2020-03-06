fe52f8b6f18b68a1c7941cc365c131db588340f8
Ovi Savescu <ovi@ovswebsites.com>
2020-03-06 18:47:22 +0200

    fixed error for missing sidebar on hidden pages that have no section

--- src/components/generic-page.js
+++ src/components/generic-page.js
@@ -103,10 +103,12 @@ function GenericPage ({_rawBody, title, section, headerImage = null}) {
         <Container sx={{maxWidth: '6xl', px: [2, 2, 4], py: [4, 5]}}s>
           {/* <Styled.root> */}
           <Flex sx={{flexWrap: 'wrap', flexDirection: 'row-reverse'}}>
-            <Box sx={{width: ['full', 'full', '3/4'], pl: [0, 0, 4]}}>
-              <div sx={{display: ['block', 'block', 'none'], mb: 4}}>
-                <PageSideNav section={section} />
-              </div>
+            <Box sx={{width: ['full', 'full', (section === 'No Section') ? 'full' : '3/4'], pl: [0, 0, 4]}}>
+              {section !== 'No Section' &&
+                <div sx={{display: ['block', 'block', 'none'], mb: 4}}>
+                  <PageSideNav section={section} />
+                </div>
+              }
               <div sx={{variant: 'styles', mb: 5}} role='main'>
                 <div sx={{mb: 4}} >
                   <Styled.h1 sx={{variant: 'text.caps'}}>{title}</Styled.h1>
@@ -114,14 +116,18 @@ function GenericPage ({_rawBody, title, section, headerImage = null}) {
                 {_rawBody && <PortableText blocks={_rawBody} />}
               </div>
             </Box>
+            {section !== 'No Section' &&
             <Box sx={{width: ['full', 'full', '1/4']}}>
               <aside sx={{
                 pr: [0, 0, 4],
                 pb: 4
               }}>
+
                 <PageSideNav section={section} />
+
               </aside>
             </Box>
+            }
           </Flex>
           {/* </Styled.root> */}
         </Container>
