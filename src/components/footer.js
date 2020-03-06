91654bda1237db447a4c8e76227510a9c5039a6c
Ovi Savescu <ovi@ovswebsites.com>
2020-03-06 17:07:23 +0200

    footer terms links added

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,7 @@
 import React from 'react' // eslint-disable-line
 import {jsx, Styled, Container, Footer as ThemeFooter} from 'theme-ui'
 import {Box, Flex} from '@theme-ui/components'
+import {Link} from 'gatsby'
 
 // components
 import SocialLinks from './socialLinks'
@@ -14,12 +15,12 @@ const Footer = () => {
   return (
     <ThemeFooter sx={{display: 'block', background: `url(${FooterBg}) repeat-x center center`, color: 'textMuted'}}>
       <Container>
-        <Flex sx={{flexWrap: 'wrap', py: [4, 4, 5, 6]}}>
+        <Flex sx={{flexWrap: 'wrap', pt: [4, 4, 5, 6], pb: [2, 3, null, null, 4]}}>
           <Box sx={{width: ['full', '1/2'], px: [0, 4], py: [4, 0], textAlign: ['center', 'right'], borderRight: ['none', '1px solid lightgray']}}>
             <div className='vcard'>
               <div className='org'><Styled.h3 as='h3' sx={{mt: 0, color: 'light'}}>Rambling Pines Day Camp</Styled.h3></div>
               <div className='tel'>
-              Call Now <a sx={{variant: 'links.footerLinks'}} href='tel:609-466-1212'>(609) 466-1212</a>
+                Call Now <a sx={{variant: 'links.footerLinks'}} href='tel:609-466-1212'>(609) 466-1212</a>
               </div>
               <a className='email' sx={{variant: 'links.footerLinks'}} href='mailto:campinfo@ramblingpines.com'>campinfo@ramblingpines.com</a>
               <div className='adr' sx={{mt: 4}}>
@@ -34,12 +35,24 @@ const Footer = () => {
           </Box>
           <Box sx={{width: ['full', '1/2'], px: 4, textAlign: 'center'}}>
             <SocialLinks />
-            <img src={AcaLogoImg} sx={{maxWidth: 'full', width: '48'}} />
+            <img src={AcaLogoImg} sx={{maxWidth: 'full', width: '48'}} alt='American Camp Association logo' />
           </Box>
         </Flex>
+        <p sx={{
+          textAlign: 'center',
+          color: 'gray.5',
+          a: {
+            color: 'gray.4'
+          }
+        }}>
+          <Link to='/terms-and-conditions/'>Terms &amp; Conditions</Link> |{' '}
+          <Link to='/privacy-policy/'>Privacy Policy</Link> |{' '}
+          <Link to='/ada-compliance/'>ADA Compliance</Link> |{' '}
+          <Link to='/request-info/'>Contact</Link>
+        </p>
       </Container>
       <div sx={{textAlign: 'center', p: 3, fontSize: 1, color: 'muted', bg: 'rgba(0,0,0,0.5)'}}>
-      &copy; {new Date().getFullYear()}, Rambling Pines Day Camp
+        &copy; {new Date().getFullYear()}, Rambling Pines Day Camp
         |
         Website by <a href='https://ovswebsites.com' sx={{color: '#f36'}}>OVS Websites</a>
       </div>
