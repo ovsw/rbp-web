fb381d9aa41df48c478ece1b713d487c845cf77c
Ovi Savescu <ovi@ovswebsites.com>
2021-05-25 10:55:21 +0300

    fixed modal video not fully visible

--- src/components/hp-sections/hero.js
+++ src/components/hp-sections/hero.js
@@ -16,126 +16,129 @@ import "../../../node_modules/react-modal-video/css/modal-video.min.css";
 const Hero = () => {
   const [playFullVideo, setPlayFullVideo] = useState(false);
   return (
-    <Box
-      as="section"
-      pt={[6, 5, 5, 5, 6]}
-      pb={[4, 5, 5, 6]}
-      mt={5}
-      sx={{
-        textAlign: "center",
-        borderBottom: "1px solid lightgrey",
-        position: "relative",
-        zIndex: "1",
-        bg: "primaryLight",
-        backgroundImage: `url('${VideoPoster}')`,
-        backgroundSize: "cover",
-        backgroundPosition: "top right"
-      }}
-      className="Hero"
-    >
-      <div
+    <div>
+      <Box
+        as="section"
+        pt={[6, 5, 5, 5, 6]}
+        pb={[4, 5, 5, 6]}
+        mt={5}
         sx={{
-          bg: "rgba(0,0,0,.3)",
-          display: ["none", "none", "block"],
-          position: "absolute",
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          zIndex: 1
-        }}
-      />
-      <div
-        sx={{
-          display: ["none", "none", "block"],
-          position: "absolute",
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          zIndex: 0
-        }}
-      >
-        <HeroVideoBg
-          playFullVideo={playFullVideo}
-          poster="https://rbpvideo-ac97.kxcdn.com/rambling-pines-camp-intro-video.jpg"
-        />
-      </div>
-      <Container
-        className="container"
-        sx={{
-          maxWidth: "5xl",
+          textAlign: "center",
+          borderBottom: "1px solid lightgrey",
           position: "relative",
-          zIndex: 10
-          // my: [3, 4, 4, 3, 5]
-          // bg: 'rgba(0,0,0,.32)'
+          zIndex: "1",
+          bg: "primaryLight",
+          backgroundImage: `url('${VideoPoster}')`,
+          backgroundSize: "cover",
+          backgroundPosition: "top right"
         }}
+        className="Hero"
       >
-        <img src={BigLogo} alt="Rambling Pines Big Logo" />
-        <Styled.h1
+        <div
           sx={{
-            textTransform: "lowercase",
-            color: "white",
-            fontWeight: "normal",
-            textShadow: "2px 2px 2px rgba(0,0,0,.5)",
-            fontSize: [3, 4],
-            fontFamily: "Lato",
-
-            mb: 0
+            bg: "rgba(0,0,0,.3)",
+            display: ["none", "none", "block"],
+            position: "absolute",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            zIndex: 1
+          }}
+        />
+        <div
+          sx={{
+            display: ["none", "none", "block"],
+            position: "absolute",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            zIndex: 0
           }}
         >
-          A Summer Day Camp Like no Other.
-        </Styled.h1>
-        <p
+          <HeroVideoBg
+            playFullVideo={playFullVideo}
+            poster="https://rbpvideo-ac97.kxcdn.com/rambling-pines-camp-intro-video.jpg"
+          />
+        </div>
+        <Container
+          className="container"
           sx={{
-            textShadow: "1px 1px 2px rgba(0,0,0,.5)",
-            fontSize: [1, 2],
-            color: "light",
-            fontFamily: "Dosis",
-            fontWeight: "light",
-            mt: 0
+            maxWidth: "5xl",
+            position: "relative",
+            zIndex: 10
+            // my: [3, 4, 4, 3, 5]
+            // bg: 'rgba(0,0,0,.32)'
           }}
         >
-          Your kids deserve the best. The best supervision, the best activities, the best friends,
-          the best memories.{" "}
-        </p>
-
-        <div sx="display: 'flex', flexWrap: 'wrap', justifyContent: 'center'">
-          <button
-            onClick={() => setPlayFullVideo(true)}
+          <img src={BigLogo} alt="Rambling Pines Big Logo" />
+          <Styled.h1
             sx={{
-              variant: "buttons.3D",
-              display: "inline-block",
-              py: 3,
-              mt: 4,
-              mr: 3
+              textTransform: "lowercase",
+              color: "white",
+              fontWeight: "normal",
+              textShadow: "2px 2px 2px rgba(0,0,0,.5)",
+              fontSize: [3, 4],
+              fontFamily: "Lato",
+
+              mb: 0
             }}
           >
-            <FaPlay sx={{ position: "relative", top: "2px" }} /> &nbsp; Watch our Full Video
-          </button>
-          <ModalVideo
-            autoplay="1"
-            isOpen={playFullVideo}
-            videoId="5rbAeDbueSA"
-            onClose={() => setPlayFullVideo(!setPlayFullVideo)}
-          />
-          <a
-            sx={{ variant: "buttons.3DAccent", display: "inline-block", mr: 3, my: 2 }}
-            href="https://calendly.com/ramblingpines/camp-tour"
-            target="_blank"
+            A Summer Day Camp Like no Other.
+          </Styled.h1>
+          <p
+            sx={{
+              textShadow: "1px 1px 2px rgba(0,0,0,.5)",
+              fontSize: [1, 2],
+              color: "light",
+              fontFamily: "Dosis",
+              fontWeight: "light",
+              mt: 0
+            }}
           >
-            <FaCalendar sx={{ position: "relative", top: "2px" }} /> Schedule a Tour
-          </a>
-          <a
-            sx={{ variant: "buttons.3D", display: "inline-block" }}
-            href="https://ramblingpines.campintouch.com/ui/forms/application/camper/App"
-            target="_blank"
-          >
-            <FaChevronRight sx={{ position: "relative", top: "2px" }} /> Enroll Now
-          </a>
-        </div>
-      </Container>
-    </Box>
+            Your kids deserve the best. The best supervision, the best activities, the best friends,
+            the best memories.{" "}
+          </p>
+
+          <div sx="display: 'flex', flexWrap: 'wrap', justifyContent: 'center'">
+            <button
+              onClick={() => setPlayFullVideo(true)}
+              sx={{
+                variant: "buttons.3D",
+                display: "inline-block",
+                py: 3,
+                mt: 4,
+                mr: 3
+              }}
+            >
+              <FaPlay sx={{ position: "relative", top: "2px" }} /> &nbsp; Watch our Full Video
+            </button>
+
+            <a
+              sx={{ variant: "buttons.3DAccent", display: "inline-block", mr: 3, my: 2 }}
+              href="https://calendly.com/ramblingpines/camp-tour"
+              target="_blank"
+            >
+              <FaCalendar sx={{ position: "relative", top: "2px" }} /> Schedule a Tour
+            </a>
+            <a
+              sx={{ variant: "buttons.3D", display: "inline-block" }}
+              href="https://ramblingpines.campintouch.com/ui/forms/application/camper/App"
+              target="_blank"
+            >
+              <FaChevronRight sx={{ position: "relative", top: "2px" }} /> Enroll Now
+            </a>
+          </div>
+        </Container>
+      </Box>
+      <ModalVideo
+        autoplay="1"
+        isOpen={playFullVideo}
+        videoId="5rbAeDbueSA"
+        onClose={() => setPlayFullVideo(!setPlayFullVideo)}
+      />
+    </div>
   );
 };
 
