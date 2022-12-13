83ce8fb6517c45326a3093685c8c1333e3f007be
Ovi Savescu <ovi@ovswebsites.com>
2022-12-13 20:48:19 +0200

    removed manual GTM code, installed missing GTM gatsby plugin

--- src/html.js
+++ src/html.js
@@ -12,13 +12,13 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-        <script dangerouslySetInnerHTML={{
+        {/* <script dangerouslySetInnerHTML={{
                 __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var
                 f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                 })(window,document,'script','dataLayer','GTM-PGMWV3V');</script>`,
             }}
-        />
+        /> */}
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
@@ -32,12 +32,12 @@ export default function HTML(props) {
         />
         {props.postBodyComponents}
 
-        <noscript dangerouslySetInnerHTML={{
+        {/* <noscript dangerouslySetInnerHTML={{
                 __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PGMWV3V" height="0" width="0"
                 style="display:none;visibility:hidden"></iframe>
                 `,
             }}
-        />
+        /> */}
       </body>
     </html>
   )
